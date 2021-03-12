5ce8dea5b66cc1d5f065e9ef597bdbc155e22fb4
sigbjorn.loland.bore@gmail.com <sigbjorn.loland.bore@gmail.com>
2021-03-12 08:12:51 -0800

    intialize potential in header file to avoid frequent memory leak.

--- src/potential/3b/energy3b.cpp
+++ src/potential/3b/energy3b.cpp
@@ -105,8 +105,8 @@ double get_3b_energy(std::string mon1, std::string mon2, std::string mon3, size_
         // =====>> END SECTION 3B_NO_GRADIENT <<=====
     } 
      else if (mon1 == "ow" and mon2 == "ow" and mon3 == "ow") {
-       OW_OW_OW_namespace::OW_OW_OW  pot(mon1, mon2, mon3);
-        energy = pot.eval(xyz1.data(), xyz2.data(), xyz3.data(), nm);
+       //OW_OW_OW_namespace::OW_OW_OW  pot(mon1, mon2, mon3);
+        energy = pot_ow_ow_ow.eval(xyz1.data(), xyz2.data(), xyz3.data(), nm);
         // =====>> END SECTION 2B_NO_GRADIENT <<=====
      }
     else {
@@ -225,8 +225,8 @@ double get_3b_energy(std::string mon1, std::string mon2, std::string mon3, size_
         // =====>> END SECTION 3B_GRADIENT <<=====
     }
   else if (mon1 == "ow" and mon2 == "ow" and mon3 == "ow") {
-    OW_OW_OW_namespace::OW_OW_OW pot(mon1, mon2,mon3);
-    energy = pot.eval(xyz1.data(), xyz2.data(), xyz3.data(), grad1.data(), grad2.data(), grad3.data(), nm, virial);
+    //OW_OW_OW_namespace::OW_OW_OW pot(mon1, mon2,mon3);
+    energy = pot_ow_ow_ow.eval(xyz1.data(), xyz2.data(), xyz3.data(), grad1.data(), grad2.data(), grad3.data(), nm, virial);
     }
   else {
         energy = 0.0;
--- src/potential/3b/energy3b.h
+++ src/potential/3b/energy3b.h
@@ -102,5 +102,7 @@ double get_3b_energy(std::string m1, std::string m2, std::string m3, size_t nm, 
                      std::vector<double> xyz2, std::vector<double> xyz3, std::vector<double> &grd1,
                      std::vector<double> &grd2, std::vector<double> &grd3, std::vector<double> *virial = 0);
 
+ const OW_OW_OW_namespace::OW_OW_OW  pot_ow_ow_ow("ow","ow","ow");
+      
 }  // namespace e3b
 #endif
